4f85c778f7f6b2196956775df765745cbdf72e87
Chris Short <61039651+durcoorigin@users.noreply.github.com>
2020-08-13 20:50:56 -0500

    22.1.6

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_CATEGORIES } from "../../utils/queries";
+import { useStoreContainer, useStoreContext } from "../../utils/GlobalState";
+import { QueryData } from "@apollo/react-hooks/lib/data/QueryData";
 
 function CategoryMenu({ setCategory }) {
+  const [state, dispatch] = useStoreContext();
+  const { categories } = state;
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
-  const categories = categoryData?.categories || [];
 
   return (
     <div>
